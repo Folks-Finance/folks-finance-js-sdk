aede3ba9ccd030d135d3b7df484e960a7df7c7c4
Gidon Katten <55109236+gidonkatten@users.noreply.github.com>
2022-03-18 17:26:55 +0000

    Rewards aggregator (#4)
    * Rewards aggregator
    
    * Fixed claim rewards comments

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export * from "./v1/lend/deposit";
 export * from "./v1/lend/liquidate";
 export * from "./v1/lend/lockAndEarn";
 export * from "./v1/lend/oracle";
+export * from "./v1/lend/rewardsAggregator";
 export * from "./v1/lend/types";
